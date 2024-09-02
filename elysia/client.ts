311a48d4d59c4dbfb93f4748d576e32e72c79479
XAN44 <nattapanlateh26@gmail.com>
2024-09-02 17:09:37 +0700

    Register Change ENV

--- elysia/client.ts
+++ elysia/client.ts
@@ -3,12 +3,10 @@ import { TElysiaApp } from ".";
 // elysia/client.ts
 
 // If you are not using Next.js v15^, you may want to set revalidate value to 0 due to default caching mechanics.
-const url = process.env.URL_DOMAIN ?? "localhost:3000";
+const url = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 
 export const elysia = treaty<TElysiaApp>(url, {
   fetch: {
     next: { revalidate: 0 },
   },
 });
-
-// export const elysia = treaty<TElysiaApp>(url);
